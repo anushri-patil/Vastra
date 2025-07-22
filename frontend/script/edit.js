d829046e6503e5c5846620e7520aceecec743a45
anushri-patil <anushripatil820@gmail.com>
2025-07-22 23:17:04 +0530

    Styling

--- frontend/script/edit.js
+++ frontend/script/edit.js
@@ -1,30 +1,30 @@
-    const id = new URLSearchParams(window.location.search).get('id');
+const id = new URLSearchParams(window.location.search).get('id');
 
-    fetch(`http://localhost:8000/products`)
-      .then(res => res.json())
-      .then(data => {
-        const product = data.find(p => p.id == id);
-        if (!product)
-             return alert("product not found");
+fetch(`http://localhost:8000/products`)
+  .then(res => res.json())
+  .then(data => {
+    const product = data.find(p => p.id == id);
+    if (!product)
+      return alert("product not found");
 
-        document.querySelector("[name='name']").value = product.name;
-        document.querySelector("[name='description']").value = product.description;
-        document.querySelector("[name='quantity']").value = product.quantity;
-        document.querySelector("[name='price']").value = product.price;
-        document.getElementById("currentImage").src = "http://localhost:8000" + product.image_url;
-      })
-      .catch(err => {
-        console.log("error product loading", err);
-      });
+    document.querySelector("[name='name']").value = product.name;
+    document.querySelector("[name='description']").value = product.description;
+    document.querySelector("[name='quantity']").value = product.quantity;
+    document.querySelector("[name='price']").value = product.price;
+    document.getElementById("currentImage").src = "http://localhost:8000" + product.image_url;
+  })
+  .catch(err => {
+    console.log("error product loading", err);
+  });
 
-    document.getElementById("editForm").onsubmit = async e => {
-      e.preventDefault();
-      const form = new FormData(e.target);
+document.getElementById("editForm").onsubmit = async e => {
+  e.preventDefault();
+  const form = new FormData(e.target);
 
-      await fetch(`http://localhost:8000/products/${id}`, {
-        method: "PUT",
-        body: form
-      });
+  await fetch(`http://localhost:8000/products/${id}`, {
+    method: "PUT",
+    body: form
+  });
 
-      window.location.href = "index.html";
-    };
+  window.location.href = "index.html";
+};
--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,49 +1,53 @@
 async function getProducts() {
-    const url = "http://localhost:8000/products";
-    try{
-        const response = await fetch(url);
-        if(!response.ok){
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const products = await response.json();
-        displayProducts(products);
-        console.log(products);
+  const url = "http://localhost:8000/products";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
-    catch(error){
-        console.log(error.message);
-    }
+    const products = await response.json();
+    displayProducts(products);
+    console.log(products);
+  }
+  catch (error) {
+    console.log(error.message);
+  }
 }
 
 function displayProducts(products) {
-    const container = document.querySelector('.products');
-    container.innerHTML = "";
+  const container = document.querySelector('.products');
+  container.innerHTML = "";
 
-    products.forEach(product => {
-        const item = document.createElement('div');
-        item.classList.add("product-card");
-        item.innerHTML = `
+  products.forEach(product => {
+    const item = document.createElement('div');
+    item.classList.add("product-card");
+    item.innerHTML = `
             <img src="http://localhost:8000${product.image_url}" alt="${product.name}">
-            <h3>${product.name}</h3>
-            <p>${product.description}</p>
+            <div class = "product-details">
+            <div class="product-name">${product.name}</div>
+            <div class="product-description">${product.description}</div>
+            <div class = card-bottom>
             <p>Quantity: ${product.quantity}</p>
             <p>Price: ${product.price}</p>
             <div class="buttons">
                 <button class="edit-btn" data-id="${product.id}">Edit</button>
                 <button class="delete-btn" data-id="${product.id}">Delete</button>
             </div>
+            </div>
+            </div>
         `;
-        container.appendChild(item);
+    container.appendChild(item);
+  });
+
+  document.querySelectorAll('.edit-btn').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const productId = e.target.dataset.id;
+      window.location.href = `edit.html?id=${productId}`;
     });
-
-    document.querySelectorAll('.edit-btn').forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            const productId = e.target.dataset.id;
-            window.location.href = `edit.html?id=${productId}`;
-        });
-    });
+  });
 
 
-    document.querySelectorAll('.delete-btn').forEach(btn => {
+  document.querySelectorAll('.delete-btn').forEach(btn => {
     btn.addEventListener('click', (e) => {
       const productId = e.target.dataset.id;
       deleteProduct(productId);
