727e620dc3dcded77b87216781570b3621c9f387
anushri-patil <anushripatil820@gmail.com>
2025-07-24 12:56:58 +0530

    added .env for MySQL configurations

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config({path: '../.env'});
+console.log("DB ENV CHECK:", process.env.DATABASE_USER);  // should print "root"
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Anushri@123",
-    database:'DB'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
 
